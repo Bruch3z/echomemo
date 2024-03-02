655c07d3d7f68ed2172d21a576aaf6c99fcf2a48
Bruch3z <83911099+Bruch3z@users.noreply.github.com>
2024-03-02 11:16:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -142,29 +142,17 @@ document.addEventListener("DOMContentLoaded", function() {
             overlay.style.display = 'none';
         });
 
-// Hide image and white overlay when tapped outside
-document.body.addEventListener('click', function(event) {
-    if (!event.target.closest('.keyimage') && !event.target.closest('.white-overlay')) {
-        document.querySelectorAll('.keyimage').forEach(function(image) {
-            image.style.display = 'none';
+        // Hide image and white overlay when tapped
+        document.body.addEventListener('click', function(event) {
+            if (!event.target.closest('.keyimage') && !event.target.closest('.white-overlay')) {
+                document.querySelectorAll('.keyimage').forEach(function(image) {
+                    image.style.display = 'none';
+                });
+                document.querySelectorAll('.white-overlay').forEach(function(overlay) {
+                    overlay.style.display = 'none';
+                });
+            }
         });
-        document.querySelectorAll('.white-overlay').forEach(function(overlay) {
-            overlay.style.display = 'none';
-        });
-    }
-});
-
-// Show image and white overlay when tapping an event block
-eventBlocks.forEach(function(eventBlock) {
-    eventBlock.addEventListener('click', function(event) {
-        const image = eventBlock.querySelector('.keyimage');
-        const overlay = eventBlock.querySelector('.white-overlay');
-        if (image && overlay) {
-            image.style.display = 'block';
-            overlay.style.display = 'block';
-        }
-    });
-});
 
         // Change image on vertical scroll
         window.addEventListener('scroll', function() {
